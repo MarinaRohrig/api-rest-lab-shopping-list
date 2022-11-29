eb48cecda2f08f88b016973bce16bbd8757363b6
Mah <marinalarissacarpesrohrig@gmail.com>
2022-11-28 21:28:56 -0300

    criação id no category.java

--- project/spring-api-shopping-list/src/main/java/br/com/futurodev/api/model/Category.java
+++ project/spring-api-shopping-list/src/main/java/br/com/futurodev/api/model/Category.java
@@ -1,5 +1,17 @@
-package br.com.futurodev.api.model;
+package br.com.futurodev.api.*;
+import java.io.Serializable;
 
-public class Category {
+@Getter
+@Setter
+@Entity
+@Table ( name = "category")
+public class Category implements Serializable {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
     
 }
